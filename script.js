f0c4c2a0c0a68205ec77e6258e9b0b7ccb811d3c
lastDunadan <last.dunadan@mail.com>
2018-01-13 18:52:30 +0100

    v_02 - clickable cells only

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 /*global console*/
 /*global $, jQuery, alert*/
 
-(function () {
+document.addEventListener('DOMContentLoaded', function () {
   'use strict';
-    
-  // Dodaj treść skryptu!
 
-}());
+  //Variables for players
+  var playerClasses = {
+    'pinkPlayer' : 'os',
+    'bluePlayer' : 'xs'
+  },
+    currentPlayer;
+
+  //Putting Xs and Os for each player
+  function cellClickHandler() {
+    
+    var playerClass = playerClasses[currentPlayer];
+
+    this.classList.remove('cell');
+    this.classList.add(playerClass);
+
+    if (currentPlayer === 'pinkPlayer') {
+      currentPlayer = 'bluePlayer';
+    } else {
+      currentPlayer = 'pinkPlayer';
+    }
+
+  }
+
+  //Run game
+  function gameStart() {
+    var cells = document.querySelectorAll('.cell-wrapper > .cell');
+
+    currentPlayer = 'pinkPlayer';
+    cells.forEach(function (cell) {
+      cell.addEventListener('click', cellClickHandler);
+    });
+  }
+
+  gameStart();
+
+});
