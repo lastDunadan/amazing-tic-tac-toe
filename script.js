a0dcd84fd8ee88dc43b067f4f097267e9a9a5f85
lastDunadan <last.dunadan@mail.com>
2018-01-14 16:20:35 +0100

    v_03 - working win/loose conditions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+/*global document*/
 /*global console*/
-/*global $, jQuery, alert*/
 
 document.addEventListener('DOMContentLoaded', function () {
   'use strict';
@@ -9,7 +9,54 @@ document.addEventListener('DOMContentLoaded', function () {
     'pinkPlayer' : 'os',
     'bluePlayer' : 'xs'
   },
-    currentPlayer;
+    currentPlayer,
+    emptyFields;
+
+  function winnerCheck() {
+    var cells = document.querySelectorAll('.cell-wrapper > div'),
+      //winning rows
+      rowOne = cells[0].className + cells[1].className + cells[2].className,
+      rowTwo = cells[3].className + cells[4].className + cells[5].className,
+      rowThree = cells[6].className + cells[7].className + cells[8].className,
+      //winning colums
+      colOne = cells[0].className + cells[3].className + cells[6].className,
+      colTwo = cells[1].className + cells[4].className + cells[7].className,
+      colThree = cells[2].className + cells[5].className + cells[8].className,
+      //winning diagonals
+      diagOne = cells[0].className + cells[4].className + cells[8].className,
+      diagTwo = cells[6].className + cells[4].className + cells[2].className;
+
+    if (rowOne === 'ososos' ||
+        rowTwo === 'ososos' ||
+        rowThree === 'ososos' ||
+        colOne === 'ososos' ||
+        colTwo === 'ososos' ||
+        colThree === 'ososos' ||
+        diagOne === 'ososos' ||
+        diagTwo === 'ososos') {
+      alert("Pink Circle Wins!");
+      return;
+    }
+
+    if (rowOne === 'xsxsxs' ||
+        rowTwo === 'xsxsxs' ||
+        rowThree === 'xsxsxs' ||
+        colOne === 'xsxsxs' ||
+        colTwo === 'xsxsxs' ||
+        colThree === 'xsxsxs' ||
+        diagOne === 'xsxsxs' ||
+        diagTwo === 'xsxsxs') {
+      alert("Blue Cross Wins!");
+      return;
+    }
+
+    //Checking for last cell to fill up
+    console.log('Cells left: ' + emptyFields);
+    if (emptyFields === 0) {
+      alert("Tie!");
+    }
+
+  }
 
   //Putting Xs and Os for each player
   function cellClickHandler() {
@@ -19,19 +66,29 @@ document.addEventListener('DOMContentLoaded', function () {
     this.classList.remove('cell');
     this.classList.add(playerClass);
 
+    emptyFields = emptyFields - 1;
+
+    //Switch player every click
     if (currentPlayer === 'pinkPlayer') {
       currentPlayer = 'bluePlayer';
     } else {
       currentPlayer = 'pinkPlayer';
     }
 
+    //Remove click event after clicking in cell
+    this.removeEventListener('click', cellClickHandler);
+
+    winnerCheck();
+
   }
 
-  //Run game
+  //Run game and conditions
   function gameStart() {
-    var cells = document.querySelectorAll('.cell-wrapper > .cell');
+    var cells = document.querySelectorAll('.cell-wrapper > div');
 
+    emptyFields = 9;
     currentPlayer = 'pinkPlayer';
+
     cells.forEach(function (cell) {
       cell.addEventListener('click', cellClickHandler);
     });
